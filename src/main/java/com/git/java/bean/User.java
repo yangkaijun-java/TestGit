e65804c39ad48293e258500c28bb08a93917ae29
yangkaijun-java <584602815@qq.com>
2021-08-03 20:45:55 +0800

    添加用户实体类

---
+++ src/main/java/com/git/java/bean/User.java
@@ -0,0 +1,20 @@
+package com.git.java.bean;
+
+public class User {
+	
+	private String username;
+	private String passwird;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPasswird() {
+		return passwird;
+	}
+	public void setPasswird(String passwird) {
+		this.passwird = passwird;
+	}
+	
+}	
